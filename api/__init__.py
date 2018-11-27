cd0309a84085f65286250106a4418d0376e82c3c
alexxsanya <alexxsanya@gmail.com>
2018-11-28 01:17:13 +0300

    Creating the API file with our various routes

---
+++ api/__init__.py
@@ -0,0 +1,27 @@
+from Flask import Blueprint
+
+bluep = Blueprint("api",__name__,url_prefix='/api/v1')
+
+@bluep.route('/red-flags', method="GET"):
+def get_all_red_flags():
+    pass
+
+@bluep.route('/red-flags/<int:red_flag_id>', method = "GET"):
+def get_this_red_flag(red_flag_id):
+    pass
+
+@bluep.route('/red-flags', method = "POST"):
+def create_this_red_flag():
+    pass
+
+@bluep.route('/red-flags/<int:red_flag_id>/location', method = "PATCH"):
+def update_red_flag_location(red_flag_id,location):
+    pass
+
+@bluep.route('/red-flags/<int:red_flag_id>/comment', method = "PATCH"):
+def update_red_flag_comment(red_flag_id, comment):
+    pass
+
+@bluep.route('/red-flags/<int:red_flag_id>', method = "DELETE"):
+def delete_red_flag(red_flag_id):
+    pass
