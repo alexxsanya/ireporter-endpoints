040e78eb02b96db45d3456655a5b109f9c8210cd
alexxsanya <alexxsanya@gmail.com>
2018-11-29 01:52:37 +0300

    [#162253837] added a test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,23 @@
-import unittest 
+import unittest
+import requests  
+from flask import jsonify , Flask
+import pytest 
 
-class Test_ireporter_endpoints(unittest.TestCase):
 
-    def test_if_create_red_flag_api_exist(self):
+# tests/test_login.py 
+import pytest
+import json
+from flask import url_for 
+
+import pytest
+import json
+from flask import url_for
+
+@pytest.mark.usefixtures('client_class')
+class TestLogin:
+    def test_if_create_red_flag_apiv1_has_no_resource(self, client):
+        assert client.get("/api/v1").status_code == 404, "This resource doesn't exits"
+
+    def test_create_this_red_flag(self, client): 
         pass
 
