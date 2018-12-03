cc19965cae07ec71c3e8455ced8e8e1a96b57e83
alexxsanya <alexxsanya@gmail.com>
2018-12-03 21:39:59 +0300

    Adding usage data to the provided routes

--- api/__init__.py
+++ api/__init__.py
@@ -3,4 +3,11 @@ from views import bluep
 
 app = Flask(__name__) 
 
+@app.route('/')
+@app.route('/api/v1')
+@app.route('/api/v1/')
+def usage_guide():
+    return "Usage Guide"
+
+
 app.register_blueprint(bluep, url_prefix="/api/v1")
