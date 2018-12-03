eebe5d167a51d237d5ed709222df80b91699401c
alexxsanya <alexxsanya@gmail.com>
2018-12-03 21:36:41 +0300

    Adding a function to test that the api return 200 upon success

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,17 +6,19 @@ import pytest
 
 # tests/test_login.py 
 import pytest
-import json
-from flask import url_for 
-
-import pytest
-import json
-from flask import url_for
+import json 
+import api
 
 @pytest.mark.usefixtures('client_class')
-class TestLogin:
-    def test_if_create_red_flag_apiv1_is_inexistent(self, client):
-        assert client.get("/api/v1").status_code == 404, "This resource doesn't exits" 
+class TestLogin:  
+    def test_apis_return_200_with_valid_parameter(self,test_client):
+        assert app.test_client().get("http://google.com").status_code == 200, "This resource should exists" 
+        assert app.test_client().get("/api/v1/redflags/1").status_code == 200, "This resource should exist"
+        
+    def test_apis_return_404_when_with_invalid_parameter(self,test_client):
+        assert self.test_client.get("/api/v1/redflagsvc").status_code == 404, "This resource doesn't exits" 
+        assert self.test_client.get("/api/v1/redflags/1").status_code == 404, "This resource doesn't exist"
 
-    def test_create_this_red_flag(self, client): 
-        pass
+    #def test_create_this_red_flag(self, client): 
+        #pass
+        
