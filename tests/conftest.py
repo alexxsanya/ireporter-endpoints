b11182fa0f5bbf4ef7ae6769f1b4ac7a01e13e0f
alexxsanya <alexxsanya@gmail.com>
2018-12-05 22:07:21 +0300

    Removed folder tests because it is noloner needed

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest 
-from flask import Flask, jsonify, Blueprint
-
-@pytest.fixture
-def app():
-    app = Flask(__name__) 
-    bluep = Blueprint("bluep", __name__)
-    app.register_blueprint(bluep, url_prefix="/api/v1")
-    return app
