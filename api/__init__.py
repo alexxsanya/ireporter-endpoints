8ca7eb01a05762a70309252c3e0c6d854e4de0b8
alexxsanya <alexxsanya@gmail.com>
2018-12-06 15:25:41 +0300

    Modified the usage guide message

--- api/__init__.py
+++ api/__init__.py
@@ -4,10 +4,12 @@ from api.views.__init__ import bluep
 app = Flask(__name__) 
 
 @app.route('/')
+@app.route('/api')
+@app.route('/api/')
 @app.route('/api/v1')
 @app.route('/api/v1/')
 def usage_guide():
-    return "Usage Guide"
+    return "<br /><h2 align='center'>Click <a href='https://github.com/alexxsanya/ireporter-endpoints/blob/develop/README.md'>Here</a> for guidance on how to use this API</h2>"
 
 
 app.register_blueprint(bluep, url_prefix="/api/v1")
