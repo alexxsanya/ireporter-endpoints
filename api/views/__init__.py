6f58c619bdc632b43c0818b9a3ef187cdb3f2a6f
alexxsanya <alexxsanya@gmail.com>
2018-12-18 14:09:17 +0300

    removing the data store here to a separate file - model folder

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,8 +1,9 @@
 from flask import Blueprint,jsonify, request, Response
 import datetime,json
+from api.models.incidents import Incidents
 
 bluep = Blueprint("bluep", __name__)
-
+incidents = Incidents.incidentsdb
 @bluep.route('/redflags', methods=['GET'])
 def get_all_red_flags():
     return jsonify({'status':200,'data': incidents})
@@ -103,39 +104,3 @@ def valid_incident(incidentObject):
         return True
     else:
         return False
-
-incidents = [
-    {
-        "idd": 1,
-        "title" : "Corrupt LC 3 in Mukono Ditrict",
-        "ttype" : "red-flag",
-        "comment" : "He is asking me for 5Million to get his approval",
-        "location" : "0.324242, 32.55338",
-        "images" : ["d:\img1.jpg","d:\img3.jpg","d:\img3.jpg"],
-        "status" : "draft",
-        "createdOn" : str(datetime.datetime.now())[:10],
-        "createdBy" : 1
-    },
-    {
-        "idd" : 2,
-        "title" : "Corrupt RDC in Gulu Ditrict",
-        "ttype" : "red-flag",
-        "comment" : "He is conienving with the chinese to steal resident's land" ,
-        "location" : "0.364242, 32.35338",
-        "images" : ["d:\img1.jpg","d:\img3.jpg","d:\img3.jpg"],
-        "status" : "rejected",
-        "createdOn" : str(datetime.datetime.now())[:10],
-        "createdBy" : 4
-    },
-    {
-        "idd" : 3,
-        "title" : "Police man asking for a bribe",
-        "ttype" : "red-flag",
-        "comment" : "Police man asking for a bribe just to see my friend who is an inmate",
-        "location" : "0.374242, 32.85338",
-        "images" : ["d:\img1.jpg","d:\img3.jpg","d:\img3.jpg"],
-        "status" : "under investigation",
-        "createdOn" : str(datetime.datetime.now())[:10],
-        "createdBy" : 4
-    }
-]
