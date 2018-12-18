3ca7feef5d6caf357d75a4c9a63e84441b01944d
alexxsanya <alexxsanya@gmail.com>
2018-12-18 17:43:42 +0300

    updating validator

--- api/utility/validate.py
+++ api/utility/validate.py
@@ -1,8 +1,10 @@
 class Validator:
-    def validate_incident(incidentObject):
+    def __init__(self):
+        pass
+    def validate_incident(self, incidentObject):
         if "idd" in incidentObject and "ttype" in incidentObject and "status" in incidentObject:
             return True 
 
-    def validate_user(userObject):
+    def validate_user(self, userObject):
         if "firstname" in userObject and "email" in userObject and "registered" in userObject:
             return True 
