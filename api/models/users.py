55d7057dc7813a69eb412e3c49672dad15ef4e8f
alexxsanya <alexxsanya@gmail.com>
2018-12-18 17:14:50 +0300

    Added models

---
+++ api/models/users.py
@@ -0,0 +1,15 @@
+import datetime,json
+class Users:
+    userdb = users = [
+    {
+        "id": 1, #integer
+        "firstname" : "Alex",
+        "lastname" : "Ssanya",
+        "othername" : "",
+        "email" : "alexxsanya@gmail.com",
+        "phoneNumber" : "+256702342257",
+        "username" : "draft",
+        "registered" : str(datetime.datetime.now()), #TimeTamp
+        "isAdmin" : False # Boolean
+    }
+]
