f5abc0b2acfc9e1991e353038b3f9dbe8f25379a
alexxsanya <alexxsanya@gmail.com>
2019-01-11 01:51:05 +0300

    Initializing JWTManager

--- api/__init__.py
+++ api/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
 from api.views.__init__ import bluep
-
+from flask_jwt_extended import JWTManager
 app = Flask(__name__) 
+
+SECRET_KEY = os.getenv('SECRETE_KEY', "precious")
+app.config['JWT_SECRET_KEY'] = SECRET_KEY
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(minutes=360)
+jwt = JWTManager(app)
 
 @app.route('/')
 @app.route('/api')
