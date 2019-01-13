adf2ec559c66fa259999f2ef6d8868049fe98008
alexxsanya <alexxsanya@gmail.com>
2019-01-13 14:36:14 +0300

    Removing duplication from the file

--- api/models/incidents.py
+++ api/models/incidents.py
@@ -12,27 +12,5 @@ class Incidents:
                         "status" : "draft",
                         "createdOn" : str(datetime.datetime.now())[:10],
                         "createdBy" : 1
-                    },
-                    {
-                        "idd" : 2,
-                        "title" : "Corrupt RDC in Gulu Ditrict",
-                        "ttype" : "red-flag",
-                        "comment" : "He is conienving with the chinese to steal resident's land" ,
-                        "location" : "0.364242, 32.35338",
-                        "images" : ["d:\img1.jpg","d:\img3.jpg","d:\img3.jpg"],
-                        "status" : "rejected",
-                        "createdOn" : str(datetime.datetime.now())[:10],
-                        "createdBy" : 4
-                    },
-                    {
-                        "idd" : 3,
-                        "title" : "Police man asking for a bribe",
-                        "ttype" : "red-flag",
-                        "comment" : "Police man asking for a bribe just to see my friend who is an inmate",
-                        "location" : "0.374242, 32.85338",
-                        "images" : ["d:\img1.jpg","d:\img3.jpg","d:\img3.jpg"],
-                        "status" : "under investigation",
-                        "createdOn" : str(datetime.datetime.now())[:10],
-                        "createdBy" : 4
                     }
                 ]
