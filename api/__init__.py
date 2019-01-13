5c2a83aeb6ed9e6479dc00b4e6b3d93304c81839
alexxsanya <alexxsanya@gmail.com>
2019-01-13 12:55:10 +0300

    Added JWT_SECRET_KEY to the app

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 from api.views.__init__ import bluep
 from flask_jwt_extended import JWTManager
+import os, datetime
 app = Flask(__name__) 
 
-SECRET_KEY = os.getenv('SECRETE_KEY', "precious")
+SECRET_KEY = os.getenv('SECRETE_KEY', "secretme")
 app.config['JWT_SECRET_KEY'] = SECRET_KEY
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(minutes=360)
 jwt = JWTManager(app)
