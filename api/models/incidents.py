aa96b9938d4069b1593f014bb94998b289760e3e
alexxsanya <alexxsanya@gmail.com>
2019-01-22 08:22:09 +0300

    [#163365147] Updated data model for testing

--- api/models/incidents.py
+++ api/models/incidents.py
@@ -3,14 +3,12 @@ import datetime,json
 class Incidents:
     incidentsdb = [
                     {
-                        "idd": 1,
                         "title" : "Corrupt LC 3 in Mukono Ditrict",
-                        "ttype" : "red-flag",
+                        "type" : "red-flag",
                         "comment" : "He is asking me for 5Million to get his approval",
                         "location" : "0.324242, 32.55338",
                         "images" : ["d:\img1.jpg","d:\img3.jpg","d:\img3.jpg"],
-                        "status" : "draft",
-                        "createdOn" : str(datetime.datetime.now())[:10],
+                        "status" : "draft", 
                         "createdBy" : 1
                     }
                 ]
