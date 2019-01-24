246624dc1a6d48e7c203b08c777ae7a3fcca971d
alexxsanya <alexxsanya@gmail.com>
2019-01-24 21:56:52 +0300

    Added code to connect to production db on heroku

--- api/utility/dbconnect.py
+++ api/utility/dbconnect.py
@@ -1,12 +1,18 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from api.utility.config import config
+from os import environ
 class Database():
     
     def __init__(self): 
         try:
             params = config() # read connection parameters from config file 
-            self.conn = psycopg2.connect(**params) #connecting
+            env = environ.get("FLASK_ENV")
+            db_url = environ.get("DATABASE_URL") 
+            if(env == "production"):
+                self.conn = psycopg2.connect(db_url) #connection for heroku
+            else:
+                self.conn = psycopg2.connect(**params) #connecting"""
             self.conn.autocommit = True
             self.cur = self.conn.cursor(cursor_factory=RealDictCursor)
             self.create_tables()
