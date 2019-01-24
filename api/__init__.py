06f6a1d3e0f45c9842dd0bc187e3bc8c782b56a5
alexxsanya <alexxsanya@gmail.com>
2019-01-24 19:46:40 +0300

    Removed unnecessary code

--- api/__init__.py
+++ api/__init__.py
@@ -4,10 +4,6 @@ from flask_mail import Mail, Message
 import os, datetime
 app = Flask(__name__) 
 
-@app.route('/')
-@app.route('/api')
-@app.route('/api/')
-@app.route('/api/v1')
 @app.route('/api/v1/')
 def usage_guide():
     return render_template("usage.html") 
