bf183481a5e85ede6519e633f39210b707803893
alexxsanya <alexxsanya@gmail.com>
2019-01-24 13:38:07 +0300

    [#163366481] Updated sender email

--- api/utility/mail_server.py
+++ api/utility/mail_server.py
@@ -3,7 +3,7 @@ mail=Mail()
 
 def send_mail_notification(**message): 
     subject = message['title'] +" << STATUS Updated >>"
-    msg = Message(subject, sender = 'alexxsanya@gmail.com', recipients = [message["email"]])
+    msg = Message(subject, sender = 'eliatboot@gmail.com', recipients = [message["email"]])
     msg.body = "Hello {}, Your incident - {} has been updated to {} ".format(message["by_name"],\
                 message["comment"],message["status"])
     mail.send(msg) 
