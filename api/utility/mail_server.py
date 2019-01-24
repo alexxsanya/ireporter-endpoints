020b885735d0b4030d34e69d62178b52bacd02c8
alexxsanya <alexxsanya@gmail.com>
2019-01-24 19:56:57 +0300

    [#163366481] Made the method to pick sender email from env variabel

--- api/utility/mail_server.py
+++ api/utility/mail_server.py
@@ -1,8 +1,9 @@
 from flask_mail import Mail, Message 
 mail=Mail()
-
+from os import environ
 def send_mail_notification(**message): 
     subject = message['title'] +" << STATUS Updated >>"
+    sender = environ.get("MAIL_USERNAME")
     msg = Message(subject, sender = 'eliatboot@gmail.com', recipients = [message["email"]])
     msg.body = "Hello {}, Your incident - {} has been updated to {} ".format(message["by_name"],\
                 message["comment"],message["status"])
