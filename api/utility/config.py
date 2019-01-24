c10a07c0d36f13ff7034abe0f8b24b991961ddc5
alexxsanya <alexxsanya@gmail.com>
2019-01-24 19:53:16 +0300

    [#163428094] Set config file to pick data from env variables

--- api/utility/config.py
+++ api/utility/config.py
@@ -1,17 +1,11 @@
-from configparser import ConfigParser
-
-def config(filename='env/database.ini', section='postgresql'):
-    # create a parser
-    parser = ConfigParser()
-    # read config file
-    parser.read(filename)
-    # get section, default to postgresql
-    db = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            db[param[0]] = param[1]
-    else:
-        raise Exception('The set section {0} not found in the {1} file specified'.format(section, filename))
- 
-    return db
+from os import environ
+def config():
+    try: 
+        db = {}
+        db['host'] = environ.get("HOST")
+        db['user'] = environ.get("USER")
+        db['password'] = environ.get("PASSWORD")
+        db['database'] = environ.get("DBNAME")
+        return db
+    except Exception:
+        raise Exception
